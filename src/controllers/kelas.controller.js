031675375872277a480d39bd85f56a17a3e9e882
fazadandy <fazadandyakmalin0@gmail.com>
2024-06-10 18:33:21 +0700

    refactor kelas controller

--- src/controllers/kelas.controller.js
+++ src/controllers/kelas.controller.js
@@ -11,30 +11,18 @@ const {
 const index = async (req, res, next) => {
   const kelas = await KelasModel.findAll({
     attributes: ["id", "name"],
-  });
-
-  const mode_pembelajaran = await ModePembelajaranModel.findAll({
-    attributes: ["id", "name", "kelas_id"],
-  });
-
-  const data = kelas.map((k) => {
-    return {
-      id: k.id,
-      name: k.name,
-      modePembelajaran: mode_pembelajaran
-        .filter((m) => m.kelas_id === k.id)
-        .map((m) => {
-          return {
-            id: m.id,
-            name: m.name,
-          };
-        }),
-    };
+    include: [
+      {
+        model: ModePembelajaranModel,
+        as: "modePembelajaran",
+        attributes: ["id", "name", "kelas_id"],
+      },
+    ],
   });
 
   return res.send({
     message: "Success",
-    data,
+    data: kelas,
   });
 };
 
